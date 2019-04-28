413b21be9ba796d387780ad1bee22655655d9ae9
kbinani <kbinani.bt@gmail.com>
2019-04-28 19:52:25 +0900

    Add example app

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by kbinani on 2019/04/28.
+//  Copyright © 2019 kbinani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- PathOps/skia_compat.mm
+++ PathOps/skia_compat.mm
@@ -1,6 +1,28 @@
 #include "include/private/SkMalloc.h"
 #include "src/core/SkSafeMath.h"
+#include "include/private/SkImageInfoPriv.h"
 
 void* sk_malloc_throw(size_t count, size_t elemSize) {
     return sk_malloc_throw(SkSafeMath::Mul(count, elemSize));
 }
+
+int SkColorTypeBytesPerPixel(SkColorType ct) {
+    switch (ct) {
+        case kUnknown_SkColorType:      return 0;
+        case kAlpha_8_SkColorType:      return 1;
+        case kRGB_565_SkColorType:      return 2;
+        case kARGB_4444_SkColorType:    return 2;
+        case kRGBA_8888_SkColorType:    return 4;
+        case kBGRA_8888_SkColorType:    return 4;
+        case kRGB_888x_SkColorType:     return 4;
+        case kRGBA_1010102_SkColorType: return 4;
+        case kRGB_101010x_SkColorType:  return 4;
+        case kGray_8_SkColorType:       return 1;
+        case kRGBA_F16Norm_SkColorType: return 8;
+        case kRGBA_F16_SkColorType:     return 8;
+        case kRGBA_F32_SkColorType:     return 16;
+    }
+    return 0;
+}
+
+int SkImageInfo::bytesPerPixel() const { return SkColorTypeBytesPerPixel(fColorType); }
