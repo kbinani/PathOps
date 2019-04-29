7e85a4b296723ec38c93bc733b26cd4ca08cd168
kbinani <kbinani.bt@gmail.com>
2019-04-30 02:40:36 +0900

    Convert conic curve to quad curves

--- PathOps/SKPath.mm
+++ PathOps/SKPath.mm
@@ -5,6 +5,7 @@
 #include "skia/include/core/SkRegion.h"
 #include "skia/include/core/SkStrokeRec.h"
 #include "skia/src/core/SkStroke.h"
+#include "skia/src/core/SkGeometry.h"
 
 @implementation SKPath
 {
@@ -123,7 +124,24 @@
                 break;
             }
             case SkPath::Verb::kConic_Verb: {
-                //TODO: conic support
+                SkScalar weight = iter.conicWeight();
+                SkPoint control = pts[1];
+                SkPoint end = pts[2];
+                if (weight == 1) {
+                    CGPathAddQuadCurveToPoint(p, nil, control.x(), control.y(), end.x(), end.y());
+                } else if (SkScalarIsFinite(weight)) {
+                    SkScalar tolerance = 1e-10; //TODO: make this configurable
+                    SkAutoConicToQuads converter;
+                    SkPoint const* quads = converter.computeQuads(pts, weight, tolerance);
+                    for (int i = 0; i < converter.countQuads(); ++i) {
+                        SkPoint p0 = quads[i * 2];
+                        SkPoint p1 = quads[i * 2 + 1];
+                        CGPathAddQuadCurveToPoint(p, nil, p0.x(), p0.y(), p1.x(), p1.y());
+                    }
+                } else {
+                    CGPathAddLineToPoint(p, nil, control.x(), control.y());
+                    CGPathAddLineToPoint(p, nil, end.x(), end.y());
+                }
                 break;
             }
             case SkPath::kDone_Verb: {
