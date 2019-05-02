f5c3d6bdcdcbe1699f699ce4760509c3e3d31eeb
kbinani <kbinani.bt@gmail.com>
2019-05-03 03:23:56 +0900

    Make SKPath extensions public

--- Source/CGPath+PathOps.swift
+++ Source/CGPath+PathOps.swift
@@ -20,25 +20,3 @@ extension CGPath {
         return p.toCGPath()
     }
 }
-
-
-extension SKPath {
-    var fillRule: CGPathFillRule? {
-        switch __fillRule() {
-        case 1:
-            return .winding
-        case 2:
-            return .evenOdd
-        default:
-            return nil
-        }
-    }
-
-    func toCGPath() -> (path: CGPath, fillRule: CGPathFillRule)? {
-        guard let fillRule = self.fillRule else {
-            return nil
-        }
-        let p = self.__toCGPath().takeRetainedValue()
-        return (path: p, fillRule: fillRule)
-    }
-}
