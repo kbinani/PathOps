421fa64569df8d81b71c040a111892357433a6f0
kbinani <kbinani.bt@gmail.com>
2019-05-02 21:06:46 +0900

    example: WIP

--- Example/DrawingCanvas.swift
+++ Example/DrawingCanvas.swift
@@ -76,181 +76,35 @@ class DrawingCanvas : UIView {
             assert(stateCount == 0)
         case .fill:
             strokes.enumerated().filter({ $0.element.alpha > 0 }).forEach({ (it) in
-                let m: Int = 6
-                if m == 9 {
-                    let p = SKPath()
-                    it.element.eachPathSegment({ (o) in
-                        var sub = SKPath(cgPath: o)
-                        strokes.dropFirst(it.offset + 1).filter({ $0.alpha <= 0 }).forEach({ (c) in
-                            sub.subtract(SKPath(cgPath: c.path))
-                        })
-                        p.union(with: sub)
-                    })
-                    ctx.saveGState()
-                    defer {
-                        ctx.restoreGState()
-                    }
-
-                    ctx.setFillColor(it.element.color.cgColor)
-                    ctx.addPath(p.toCGPath().takeRetainedValue())
-                    ctx.fillPath()
-                } else if m == 8 {
-                    let p = SKPath(cgPath: it.element.path)
-                    strokes.dropFirst(it.offset + 1).filter({ $0.alpha <= 0 }).forEach({ (c) in
-                        c.eachPathSegment({ (cp) in
-                            p.subtract(SKPath(cgPath: cp))
-                        })
-                    })
-                    ctx.saveGState()
-                    defer {
-                        ctx.restoreGState()
-                    }
-                    ctx.setFillColor(it.element.color.cgColor)
-                    ctx.addPath(p.toCGPath().takeRetainedValue())
-                    ctx.fillPath()
-                } else if m == 7 {
-                    var p = SKPath(cgPath: it.element.hairlinePath).copyStroking(withWidth: it.element.width, lineCap: .round, lineJoin: .round, miterLimit: 0, resolutionScale: 1e-10)
-                    strokes.dropFirst(it.offset + 1).filter({ $0.alpha <= 0 }).forEach({ (c) in
-                        let cp = SKPath(cgPath: c.hairlinePath).copyStroking(withWidth: c.width, lineCap: .round, lineJoin: .round, miterLimit: 0, resolutionScale: 1e-10)
-                        p.subtract(cp)
-                    })
-                    ctx.saveGState()
-                    defer {
-                        ctx.restoreGState()
-                    }
-
-                    ctx.setFillColor(it.element.color.cgColor)
-                    ctx.addPath(p.toCGPath().takeRetainedValue())
-                    ctx.fillPath()
-                } else if m == 6 {
-                    //ok?
-                    var p = SKPath(cgPath: it.element.path)
-                    strokes.dropFirst(it.offset + 1).filter({ $0.alpha <= 0 }).forEach({ (c) in
-                        p.subtract(SKPath(cgPath: c.path))
-                    })
-
-                    ctx.saveGState()
-                    defer {
-                        ctx.restoreGState()
-                    }
-
-                    ctx.setFillColor(it.element.color.cgColor)
-                    ctx.addPath(p.toCGPath().takeRetainedValue())
-                    ctx.fillPath()
-                } else if m == 5 {
-                    let p = it.element.hairlinePath
-                    let sk = SKPath(cgPath: p)
-                    let stroked = sk.copyStroking(withWidth: width, lineCap: .round, lineJoin: .round, miterLimit: 0, resolutionScale: 1e-10)
-                    strokes.dropFirst(it.offset + 1).filter({ $0.alpha <= 0 }).forEach({ (c) in
-                        c.eachPathSegment({ (cp) in
-                            let skcp = SKPath(cgPath: cp)
-                            stroked.subtract(skcp)
-                        })
-                    })
-                    ctx.setFillColor(it.element.color.cgColor)
-                    ctx.addPath(stroked.toCGPath().takeRetainedValue())
-                    ctx.fillPath()
-                } else if m == 4 {
-                    let p = CGMutablePath()
-                    it.element.eachPathSegment({ (o) in
-                        var sub = o
-                        strokes.dropFirst(it.offset + 1).filter({ $0.alpha <= 0 }).forEach({ (c) in
-                            sub = sub.subtract(path: c.path)
-                        })
-                        p.addPath(sub)
-                    })
-                    ctx.saveGState()
-                    defer {
-                        ctx.restoreGState()
-                    }
-
-                    ctx.setFillColor(it.element.color.cgColor)
-                    ctx.addPath(p)
-                    ctx.fillPath()
-                } else if m == 3 {
-                    //NG
-                    var p: CGPath = CGMutablePath()
-                    it.element.eachPathSegment({ (o) in
-                        var sub = o
-                        strokes.dropFirst(it.offset + 1).filter({ $0.alpha <= 0 }).forEach({ (c) in
-                            sub = sub.subtract(path: c.path)
-                        })
-                        p = p.union(with: sub)
-                    })
-                    ctx.saveGState()
-                    defer {
-                        ctx.restoreGState()
-                    }
-
-                    ctx.setFillColor(it.element.color.cgColor)
-                    ctx.addPath(p)
-                    ctx.fillPath()
-                } else if m == 2 {
-                    //NG
-                    var p = it.element.path
-                    strokes.dropFirst(it.offset + 1).filter({ $0.alpha <= 0 }).forEach({ (c) in
-                        c.eachPathSegment({ (c) in
-                            p = p.subtract(path: c)
-                        })
-                    })
-                    ctx.saveGState()
-                    defer {
-                        ctx.restoreGState()
-                    }
-
-                    ctx.setFillColor(it.element.color.cgColor)
-                    ctx.addPath(p)
-                    ctx.fillPath()
-                } else if m == 1 {
-                    //NG
-                    var p: CGPath = CGMutablePath()
-                    it.element.eachPathSegment({ (o) in
-                        p = p.union(with: o)
-                    })
-                    strokes.dropFirst(it.offset + 1).filter({ $0.alpha <= 0 }).forEach({ (c) in
-                        c.eachPathSegment({ (cp) in
-                            p = p.subtract(path: cp)
-                        })
-                    })
-                    ctx.saveGState()
-                    defer {
-                        ctx.restoreGState()
-                    }
-
-                    ctx.setFillColor(it.element.color.cgColor)
-                    ctx.addPath(p)
-                    ctx.fillPath()
-                } else if m == 0 {
-                    //NG
-                    var p = it.element.path
-                    strokes.dropFirst(it.offset + 1).filter({ $0.alpha <= 0 }).forEach({ (c) in
-                        p = p.subtract(path: c.path)
-                    })
-
-                    ctx.saveGState()
-                    defer {
-                        ctx.restoreGState()
-                    }
-
-                    ctx.setFillColor(it.element.color.cgColor)
-                    ctx.addPath(p)
-                    ctx.fillPath()
-                }
-            })
-        case .frame:
-            strokes.enumerated().filter({ $0.element.alpha > 0 }).forEach({ (it) in
-                let clearPath = CGMutablePath()
+                var p = SKPath(cgPath: it.element.path)
                 strokes.dropFirst(it.offset + 1).filter({ $0.alpha <= 0 }).forEach({ (c) in
-                    clearPath.addPath(c.path)
+                    p.subtract(SKPath(cgPath: c.path))
                 })
+
                 ctx.saveGState()
                 defer {
                     ctx.restoreGState()
                 }
-                let p = it.element.path.subtract(path: clearPath)
+
+                ctx.setFillColor(it.element.color.cgColor)
+                ctx.addPath(p.toCGPath().takeRetainedValue())
+                ctx.fillPath()
+            })
+        case .frame:
+            strokes.enumerated().filter({ $0.element.alpha > 0 }).forEach({ (it) in
+                var p = SKPath(cgPath: it.element.path)
+                strokes.dropFirst(it.offset + 1).filter({ $0.alpha <= 0 }).forEach({ (c) in
+                    p.subtract(SKPath(cgPath: c.path))
+                })
+
+                ctx.saveGState()
+                defer {
+                    ctx.restoreGState()
+                }
+
                 ctx.setLineWidth(1 / UIScreen.main.scale)
                 ctx.setStrokeColor(it.element.color.cgColor)
-                ctx.addPath(p)
+                ctx.addPath(p.toCGPath().takeRetainedValue())
                 ctx.strokePath()
             })
         }
